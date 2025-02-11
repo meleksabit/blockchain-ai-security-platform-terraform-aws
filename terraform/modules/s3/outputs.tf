fa9c249bc09435b7b4c42756d311275224a77407
meleksabit <angel3.85bg@gmail.com>
2025-02-11 12:54:16 +0200

    edit terraform infra

--- terraform/modules/s3/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "ai_model_storage_bucket" {
-  value = aws_s3_bucket.ai_model_storage.bucket
-}
-
-output "logs_exports_bucket" {
-  value = aws_s3_bucket.logs_exports.bucket
-}
--- terraform/provider.tf
+++
@@ -1,7 +0,0 @@
-# ----------------------------------------------------------------------------
-# This file is used to configure the provider for the Terraform configuration.
-# ----------------------------------------------------------------------------
-provider "aws" {
-  region  = var.aws_region  # AWS region
-  profile = var.aws_profile # AWS CLI profile
-}
